856a5ec2c96e6ad7f8a048ac1dffaabeda9733ff
quentin Fonck <quentin.fonck@gmail.com>
2017-01-17 22:24:48 +0100

    selected recipe Behavior added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe';
 
 @Component({
@@ -8,9 +8,14 @@ import { Recipe } from '../recipe';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipe = new Recipe('Dummy', 'Dummy', 'https://s-media-cache-ak0.pinimg.com/736x/3c/f9/28/3cf9288e26e5e53f0c9b987b9cab044d.jpg');
 
   constructor() { }
+
+  onSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  }
 
   ngOnInit() {
   }
