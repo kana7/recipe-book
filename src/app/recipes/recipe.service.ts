04f98db57d1ffc3315ccfb6d4a44f94ad68a9add
Quentin Fonck <quentin.fonck@internet.lu>
2017-01-23 14:10:37 +0100

    Adding ingredients in init recipes data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,13 @@
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient';
 
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg', []),
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1),
+    ]),
     new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ];
 
